16ac45a7f120ff6c91dac3b25a779b882ff67d00
hosted-fornet <hosted-fornet@protonmail.com>
2025-01-06 21:27:48 -0800

    fix spinner

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,3 +1,3 @@
 export const Spinner = () => (
-  <div className="animate-spin rounded-full h-4 w-4 border-[3px] border-primary-foreground/30 border-t-primary-foreground" />
+  <div className="animate-spin rounded-full h-4 w-4 border-[3px] border-current/30 border-t-current" />
 );
