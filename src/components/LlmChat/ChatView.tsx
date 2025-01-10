0a38eaf537f573c6bd13f10b3878ee334017f61e
hosted-fornet <hosted-fornet@protonmail.com>
2025-01-09 21:22:48 -0800

    fix: align chat messages to input box by removing excess padding

--- src/components/LlmChat/ChatView.tsx
+++ src/components/LlmChat/ChatView.tsx
@@ -614,8 +614,8 @@ export const ChatView: React.FC = () => {
 
   return (
     <div className="flex flex-col h-full relative">
-    <div ref={chatContainerRef} className="h-[calc(100vh-8rem)] overflow-y-auto p-4 pb-[100px] md:pb-[120px]">
-        <div className="space-y-4">
+      <div ref={chatContainerRef} className="h-[calc(100vh-8rem)] overflow-y-auto p-4">
+        <div className="space-y-4 mb-4">
           {activeConversation.messages.map((message, index) => (
             renderMessage(message, index)
           ))}
