47cdd09ba4d3155f566fbbbb517831c5a41dc316
hosted-fornet <hosted-fornet@protonmail.com>
2025-01-14 11:08:39 -0800

    rename kibitz; TODO: tidy up favicon stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'kinode-llm-chat',
-  description: 'Chat interface for LLMs',
+  title: 'Kibitz',
+  description: 'Chat with LLMs that use tools',
 };
 
 export default function RootLayout({
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg" />
+      </head>
       <body className={`${inter.className} bg-background min-h-screen`}>
         <ThemeProvider
           attribute="class"
