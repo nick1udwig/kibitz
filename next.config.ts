afc26989450e0c21950e172a49c949a1426997b6
hosted-fornet <hosted-fornet@protonmail.com>
2025-01-17 14:09:31 -0800

    allow setting base url path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // Set your base path here. For example: '/kibitz' or '/my-app'
+  // Leave it as undefined or remove it to use the root path
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export default nextConfig;
